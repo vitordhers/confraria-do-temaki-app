61ee7901fc05d286f6c53bfc29560baf43406609
Vitor Dhers <vitor.dhers@gmail.com>
2022-05-03 23:34:41 -0300

    add cloudfront images

--- src/app/components/gallery/gallery.component.ts
+++ src/app/components/gallery/gallery.component.ts
@@ -7,6 +7,14 @@ import { navigateExternalLink } from '../../shared/functions/navigate-external-l
   styleUrls: ['./gallery.component.scss'],
 })
 export class GalleryComponent implements OnInit {
+  galleryImgs = [
+    'https://d1sebrm9qxhird.cloudfront.net/product_img/image1.png',
+    'https://d1sebrm9qxhird.cloudfront.net/product_img/image2.png',
+    'https://d1sebrm9qxhird.cloudfront.net/product_img/image3.png',
+    'https://d1sebrm9qxhird.cloudfront.net/product_img/image4.png',
+    'https://d1sebrm9qxhird.cloudfront.net/product_img/image5.png',
+    'https://d1sebrm9qxhird.cloudfront.net/product_img/image6.png',
+  ];
   navigateExternalLink = navigateExternalLink;
 
   constructor() {}
